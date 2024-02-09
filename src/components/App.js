1bfc129010bc2cb2e4b87f1ea6cf58132e9fef33
Rachel <butterfly.liaw@gmail.com>
2024-02-09 06:43:52 +0200

    Refactor code

--- src/components/App.js
+++ src/components/App.js
@@ -10,28 +10,52 @@ function App() {
 
   const bgImages = [
     {
-      alt:'bg_2022newyear_01.png',
-      src: './images/bg_2022newyear_01.png'
+      alt:'dragon-bg-1',
+      src: './images/dragon-bg-1.png'
     },
     {
-      alt:'bg_2022newyear_02.png',
-      src: './images/bg_2022newyear_02.png'
+      alt:'dragon-bg-2',
+      src: './images/dragon-bg-2.png'
     },
     {
-      alt:'bg_2022newyear_03.png',
-      src: './images/bg_2022newyear_03.png'
-    },
+      alt:'dragon-bg-3',
+      src: './images/dragon-bg-3.png'
+    }
   ]
 
   const stickers = [
     {
       alt:'sticker_hand',
-      src: './images/sticker_hand.png'
+      src: './images/dragon-head.png'
     },
     {
-      alt:'sticker_hat',
-      src: './images/sticker_hat.png'
+      alt:'sticker_tail',
+      src: './images/dragon-tail.png'
     },
+    {
+      alt:'sticker-money-1',
+      src: './images/sticker-money-1.png'
+    },
+    {
+      alt:'sticker-money-2',
+      src: './images/sticker-money-2.png'
+    },
+    {
+      alt:'sticker-dragon-1',
+      src: './images/sticker-dragon-1.png'
+    },
+    {
+      alt:'sticker-dragon-2',
+      src: './images/sticker-dragon-2.png'
+    },
+    {
+      alt:'sticker-dragon-3',
+      src: './images/sticker-dragon-3.png'
+    },
+    {
+      alt:'sticker-dragon-4',
+      src: './images/sticker-dragon-4.png'
+    }
   ]
 
   return (
