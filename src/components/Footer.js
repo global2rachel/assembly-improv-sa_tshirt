9733816a04f6470351b1a2ad5a3dd5d8bf9a1f14
global2rachel <global2rachel@gmail.com>
2024-10-17 16:26:25 +0200

    update text and favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import React from "react";
 const Footer = (props)=>{
     return (
         <div className="text-center">
-            <a type="button" className="text-center mt-2 text-white" href="https://github.com/rachel-liaw" target="_blank" rel="noreferrer">Copyright
-                © 2024 by Global Rachel</a>
+            <a type="button" className="text-center mt-2 text-white" href="https://www.theassemblyimprov.co.za/" target="_blank" rel="noreferrer">Copyright
+                © The Assembly Improv SA</a>
+<h3 className="text-center mt-2 text-white"> | 
+Made with ❤️ by Rachel & ChapGPT</h3>
         </div>
     )
 }
