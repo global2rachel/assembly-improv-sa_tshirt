19728200222c2bf635221357426fe33e86092f6d
global2rachel <global2rachel@gmail.com>
2024-10-18 14:57:22 +0200

    modal: renderTitle dynamically

--- src/components/CustomModal.js
+++ src/components/CustomModal.js
@@ -17,11 +17,23 @@ const CustomModal = (props)=>{
       )
   }
 
+  const renderTitle = ()=>{
+    if(props.colorName ||  props.logoType) return (
+      <>
+      
+      <h2>Your Assembly Improv T-shirt Order</h2><br/>
+                <h3><strong>T-shirt Color:</strong> {props.colorName} <br/></h3>
+                <h3><strong>Logo Name:</strong>  {props.logoType}</h3></>
+    )
+
+    else return props.title;
+  }
+
 
     return(
         <Modal size={props.size} show={props.show} onHide={props.handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{props.title}</Modal.Title>
+          <Modal.Title>{renderTitle()}</Modal.Title>
         </Modal.Header>
         <Modal.Body style={{overflow: 'scroll', height: props.height}}>
             {props.children}
